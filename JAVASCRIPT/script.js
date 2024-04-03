99fa7436aed129698de0e3784cbdb670e049a421
KV <kizzyvortex@gmail.com>
2024-04-03 15:31:41 +0100

    update

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -55,7 +55,17 @@ window.addEventListener('scroll', () => {
 });
 
 
-// calendly trigger
+// change nav on scroll
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
 
  
 
